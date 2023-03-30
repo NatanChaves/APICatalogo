1a070db7d25a228afbbc9e2012575d76015e3879
Natan Inacio <nataninacio1chaves@gmail.com>
2023-03-29 22:19:47 -0300

    Add project files.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,90 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.ToList();
+        }
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategorias()
+        {
+            var categorias = _context.Categorias.AsNoTracking().Include(p=> p.Produtos).ToList();
+            if (categorias is null)
+            {
+                return NotFound("Categorias não encontradas");
+            }
+            return categorias;
+        }
+
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(x => x.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound();
+            }
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+                return BadRequest();
+
+            _context.Categorias.Add(categoria);
+
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+                return BadRequest();
+
+            _context.Entry(categoria).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+            if (categoria is null)
+                return NotFound();
+
+            _context.Categorias.Remove(categoria);
+
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20230328015102_PopulaProdutos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values('Coca-Cola', 'Refrigerante', 6.70, 'cocacola.jpg', 10, now(), 1);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos;");
+        }
+    }
+}
